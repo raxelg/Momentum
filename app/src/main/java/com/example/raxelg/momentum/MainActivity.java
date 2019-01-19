45a8cdaa2c168387c553a04239c26dd241c16161
raxelg <raxelg@mit.edu>
2019-01-18 20:26:24 -0500

    location permissions

--- app/src/main/java/com/example/raxelg/momentum/MainActivity.java
+++ app/src/main/java/com/example/raxelg/momentum/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.raxelg.momentum;
 
-
 import android.os.Build;
 import android.os.VibrationEffect;
 import android.support.v7.app.AppCompatActivity;
@@ -8,22 +7,30 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.os.Vibrator;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Intial objects
     Button vibrateButton;
     Vibrator vibrator;
-    int distance = 0;
+    TextView textView;
+
+    //Hard-coded variables
     long[] mVibratePattern = new long[]{0, 1000, 1000, 1000, 1000, 1000};
     int[] mAmplitudes = new int[]{0, 25, 75, 125, 175, 255};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Necessary object identification/authorization
+        textView = (TextView)findViewById(R.id.coordinateText);
         vibrateButton = findViewById(R.id.vibrateButton);
         vibrator = (Vibrator)getSystemService(VIBRATOR_SERVICE);
 
+        //Button vibrates continuously
         vibrateButton.setOnClickListener(new Button.OnClickListener(){
             public void onClick(View v){
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
