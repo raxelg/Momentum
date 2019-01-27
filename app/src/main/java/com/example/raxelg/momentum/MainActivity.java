05c5307be4a8b61d42b002f605bc596914089bb0
laBruji <htromero66@gmail.com>
2019-01-26 21:31:17 -0500

    vibration amplitude set according to distance No buttons

--- app/src/main/java/com/example/raxelg/momentum/MainActivity.java
+++ app/src/main/java/com/example/raxelg/momentum/MainActivity.java
@@ -1,37 +1,47 @@
 package com.example.raxelg.momentum;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Build;
 import android.os.VibrationEffect;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.os.Vibrator;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.util.Random;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import static java.lang.Boolean.FALSE;
-import static java.lang.Boolean.TRUE;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity {
 
     //Initial objects
-    private Button vibrateButton;
-    private Button stopButton;
     private Vibrator vibrator;
-    TextView textView;
-    private LocationManager locationManager;
-    String provider;
+    private TextView textView;
+    private FusedLocationProviderClient client;
+    private Location mLastLocation;
+    private LocationRequest mLocationRequest;
+    private LocationCallback locationCallBack;
+    private static final int MY_PERMISSION_REQUEST_FINE_LOCATION = 101;
+    private boolean updatesOn = true;
+    private TextView distanceTextView;
+    private TextView vibrationTextView;
+    double lat2 = 42.355139;
+    double long2 = -1*71.1008134;
+    float [] results = new float[5];
+    int vibrationPower;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,124 +50,100 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         //Necessary object identification/authorization
         textView = findViewById(R.id.coordinateText);
-        vibrateButton = findViewById(R.id.vibrateButton);
-        stopButton = findViewById(R.id.b_stop);
         vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        provider = locationManager.getBestProvider(new Criteria(), false);
+        distanceTextView = findViewById(R.id.distanceText);
+        vibrationTextView = findViewById(R.id.vibrationText);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
 
 
-        /*    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                    != PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                            != PackageManager.PERMISSION_GRANTED) {
-                return;
-            }*/
-        //Location location = locationManager.getLastKnownLocation(provider);
 
-        stopButton.setOnClickListener(new Button.OnClickListener(){
-            public void onClick(View v){
-                vibrator.cancel();
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            client.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        mLastLocation = location;
+                        textView.setText("Longitude: " + String.valueOf(location.getLatitude()) + "Latitude: " + String.valueOf(location.getLatitude()));
+                    }
+                }
+            });
+        }else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_FINE_LOCATION);
             }
-        });
+        }
 
+        locationCallBack = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                for(Location location: locationResult.getLocations()){
+                    if(location != null){
+                        textView.setText("Longitude: " + String.valueOf(location.getLongitude()) + "\n" + "Latitude: " + String.valueOf(location.getLatitude()));
+                        location.distanceBetween(location.getLatitude(), location.getLongitude(), lat2,long2,results);
+                        distanceTextView.setText("Distance: " + String.valueOf(results[0]*3.28084));
+                        vibrationPower = distance_to_vibration.vibration_strength(results[0] * 3.28084);
+                        vibrationTextView.setText(String.valueOf(vibrationPower));
 
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            if (vibrator.hasAmplitudeControl()) {
+                                if((results[0] * 3.28084) < 30) {
+                                    VibrationEffect pattern = VibrationEffect.createWaveform(new long[]{1000, 2000}, new int[]{0, 0, distance_to_vibration.vibration_strength(results[0] * 3.28084)}, 1);
+                                    vibrationTextView.setText(String.valueOf(vibrationPower));
+                                    vibrator.vibrate(pattern);
+                                } else if((results[0] *3.28084) > 30 || (results[0] *3.28084) < 2 ){
+                                    vibrator.cancel();
+                                }
 
+                            } else {
+                                vibrator.vibrate(VibrationEffect.createWaveform(new long[]{0, 4}, -1));
+                            }
+                        }
+                    }
+                }
+            }
+        };
 
-        //Button vibrates continuously
-        vibrateButton.setOnClickListener(new Button.OnClickListener() {
-                                             public void onClick(View v) {
-                                                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                                                     if (vibrator.hasAmplitudeControl()) {
+    }
 
-                                                         //permission for location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_FINE_LOCATION:
 
-                                                         if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                                                                 != PackageManager.PERMISSION_GRANTED &&
-                                                                 ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
-                                                                         != PackageManager.PERMISSION_GRANTED) {
-                                                             return;
-                                                         }
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //permission was granted do nothing and carry on
 
-                                                         Location location;
-                                                         double lat1, lon1, lat2, lon2, distance;
-                                                         int vibrationStrength;
-                                                         VibrationEffect pattern;
-                                                         Random carPosition = new Random();
+                } else {
+                    Toast.makeText(getApplicationContext(), "This app requires location permissions to be granted", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
 
-                                                         do{
-                                                             location = locationManager.getLastKnownLocation(provider);
-                                                             onLocationChanged(location);
-                                                             lat1 = location.getLatitude();
-                                                             lon1 = location.getLongitude();
-                                                             lat2 = lat1;
-                                                             //rangeMin + (rangeMax - rangeMin) * r.nextDouble(); Cambridge values
-                                                             lon2 = -71.1 + (0.1 * carPosition.nextDouble());
-
-                                                             distance = distance_to_vibration.distanceBetweenInFt(lat1, lon1, lat2, lon2);
-
-                                                             vibrationStrength = distance_to_vibration.vibration_strength(distance);
-                                                             pattern = VibrationEffect.createWaveform(new long[] {0, 1000}, new int[]{vibrationStrength,0}, -1);
-                                                             vibrator.vibrate(pattern);
-
-                                                        } while(distance_to_vibration.distanceBetweenInFt(lat1, lon1, lat2, lon2) > 2 || distance_to_vibration.distanceBetweenInFt(lat1, lon1, lat2, lon2) <= 30);
-
-                                                     }
-                                                 } else {
-                                                     vibrator.vibrate(400);
-                                                 }
-                                             }
-                                         }
-        );
-
-
+                break;
+        }
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        if (updatesOn) startLocationUpdates();
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED){
-            return;
+    }
+
+    private void startLocationUpdates(){
+        if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            client.requestLocationUpdates(mLocationRequest,locationCallBack,null);
+        }else{
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},MY_PERMISSION_REQUEST_FINE_LOCATION);
+            }
         }
-        locationManager.requestLocationUpdates(provider, 400, 1, this);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
-                        != PackageManager.PERMISSION_GRANTED){
-            return;
-        }
-        locationManager.removeUpdates(this);
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        double longitude = location.getLongitude();
-        double latitude = location.getLatitude();
-        textView.setText("Longitude: " + longitude + "\n" + "Latitude: " + latitude);
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
-    }
 }
--- app/src/main/java/com/example/raxelg/momentum/distance_to_vibration.java
+++ app/src/main/java/com/example/raxelg/momentum/distance_to_vibration.java
@@ -9,31 +9,38 @@ import android.widget.Button;
 import android.os.Vibrator;
 import android.widget.Toast;
 
+import java.text.DecimalFormat;
+
 
 public class distance_to_vibration {
 
-    public static double distanceBetweenInFt(double lat1, double lon1, double lat2, double lon2) { //lat1 and lon1 are the location of the phone
-        double R = 6371; //radius of earth in km
-        lat1 = Math.toRadians(lat1);
-        lon1 = Math.toRadians(lon1);
-        lat2 = Math.toRadians(lat2);
-        lon2 = Math.toRadians(lon2);
+    public static double distanceBetween(double lat1, double lon1, double lat2, double lon2) {
+        int Radius = 6371;// radius of earth in Km
 
-        double dlat = lat2 - lat1;
-        double dlon = lon2 - lon1;
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(lat1))
+                * Math.cos(Math.toRadians(lat2)) * Math.sin(dLon / 2)
+                * Math.sin(dLon / 2);
+        double c = 2 * Math.asin(Math.sqrt(a));
+        double valueResult = Radius * c* 3280.8399;
+        //double km = valueResult / 1;
+        //DecimalFormat newFormat = new DecimalFormat("####");
+        //int kmInDec = Integer.valueOf(newFormat.format(km));
+        //double meter = valueResult % 1000;
+        //int meterInDec = Integer.valueOf(newFormat.format(meter));
+        //Log.i("Radius Value", "" + valueResult + "   KM  " + kmInDec
+                //+ " Meter   " + meterInDec);
 
-        double x = dlon * Math.cos(lat1 + lat2) / 2;
-
-        //1km=3,280.839895ft
-
-        return 3280.839895* R * Math.sqrt(x * x + dlat * dlat);
-
+        return valueResult ;
     }
+
 
 
     public static int vibration_strength(double distance) {
 
-        return (int) (-1 * distance * (255.0 / 30.0)) + 255;
+        return (int) ((-1 * distance * (255.0 / 30.0)) + 255);
 
     }
 
